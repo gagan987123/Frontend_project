86a2542565b833495be321f9161ed8460342b8de
Gagandeep singh <gaganarora987123@gmail.com>
2023-12-30 00:18:03 +0530

    added teacher section and some update

--- script.js
+++ script.js
@@ -14,15 +14,38 @@ var swiper = new Swiper(".course-slider", {
     },
     breakpoints: {
       640: {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 20,
       },
       768: {
-        slidesPerView: 4,
+        slidesPerView: 2,
         spaceBetween: 40,
       },
       1024: {
-        slidesPerView: 5,
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".teacher-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
         spaceBetween: 50,
       },
     },

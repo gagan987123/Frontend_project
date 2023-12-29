c6c596dce297cb5842ff7aa1d3f7d17d67275987
Gagandeep singh <gaganarora987123@gmail.com>
2023-12-30 01:45:53 +0530

    added reviews section

--- script.js
+++ script.js
@@ -50,3 +50,25 @@ var swiper = new Swiper(".course-slider", {
       },
     },
   });
+  var swiper = new Swiper(".reviews-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
